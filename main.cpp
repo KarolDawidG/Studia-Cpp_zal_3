ee028d53f5069437c35418867d8f06df3a851d37
KarolDawidG <karoldawidg@gmail.com>
2022-05-21 16:17:01 +0200

    log 3 - tabliczka mnozenia

--- main.cpp
+++ main.cpp
@@ -4,15 +4,18 @@ using namespace std;
 
 int main()
 {
-
-
-for (int i=1; i<=3; i++)
-{
-for(int a=1; a<=6; a++)
-{
-cout << "x";
+    cout << "Tabliczka mnozenia" << endl;
+    cout<<endl;
+    int n;
+    cout <<"Podaj wartosc n z przedzialu <1, 10>: "<<endl;
+    cin>>n;
+    for (int i=1; i<=n; i++)
+    {
+        for (int j=1; j<=n; j++)
+        {
+            cout<<i*j<<"\t";
+        }
+        cout<<endl;
+    }
+    return 0;
 }
-cout<<endl;
-}
-}
-
