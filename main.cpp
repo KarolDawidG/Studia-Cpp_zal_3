786630dcadfd3a4b65a8b3803ed53b24559a3783
KarolDawidG <karoldawidg@gmail.com>
2022-05-25 16:13:52 +0200

    Log 7: Zadanie 3

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 // zadanie zaliczeniowe 1.3
 #include <iostream>
+#include <math.h>
+#include <iomanip>
 
 using namespace std;
 
 int main()
-{{
+{
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+    {
 
     cout << "ZADANIE NR 1." << endl;
     cout << "Tabliczka mnozenia" << endl;
@@ -20,15 +25,18 @@ int main()
             {
                 cout<<i*j<<"\t";
             }
-            cout<<endl;
+            cout<<endl<<endl;
         }
     }
     else
     {
-        cout << "Liczba n spoza przedzialu. " << endl;
-    }}
+        cout << "Liczba n spoza przedzialu. " << endl<<endl;
+    }
+    }
+/////////////////////////////////////////////////////////////////////////////////////////////
 
 
+/////////////////////////////////////////////////////////////////////////////////////////////
    {
     int h, n, a, x;
     cout << "ZADANIE NR 2." << endl;
@@ -42,8 +50,30 @@ int main()
     for(int i = 1; i<=n; i++)
     {
        x=a+i*h;
-       cout<<"Funkcja y(x)=3^2+3x-1 dla x = "<<x<<" wynosi: "<<3*x*x+3*x-1<<"\t"<<endl;
-    }}
+       cout<<"Funkcja y(x)=3^2+3x-1 dla x = "<<x<<" wynosi: "<<3*x*x+3*x-1<<"\t"<<endl<<endl;
+    }
+    }
+/////////////////////////////////////////////////////////////////////////////////////////////
+    {
+        int nn=1;
+        int licznik2=0;
+        float suma2=0;
+        float srednia_liczb;
+        cout << "ZADANIE NR 3." << endl;
+        cout<<"Podaj liczbe calkowita: "<<endl;
+        while(nn!=0)
+        {
+            cin>>nn;
+            licznik2++;
+            suma2+=nn;
+            cout<<"Podaj kolejna liczbe calkowita: "<<endl;
+        }
+        srednia_liczb=suma2/(licznik2-1);
+        cout<<"Suma liczb calkowitych wynosi: "<<suma2<<endl;
+        cout<<"Srednia liczb calkowitych wynosi: "<<setprecision(2)<<srednia_liczb<<endl<<endl;
+
+    }
+
 
     return 0;
 }
